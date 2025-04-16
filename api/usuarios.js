afc107ccef9db38d6bf80dcd01266f7d9726afc6
ConectaServe <conectaserve@gmail.com>
2025-04-16 12:46:56 -0300

    Create usuarios.js

---
+++ api/usuarios.js
@@ -0,0 +1,19 @@
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
+
+if (!getApps().length) {
+  initializeApp({ credential: cert(serviceAccount) });
+}
+const db = getFirestore();
+
+export default async function handler(req, res) {
+  try {
+    const snapshot = await db.collection("usuarios").get();
+    const usuarios = snapshot.docs.map(doc => doc.data());
+    res.status(200).json(usuarios);
+  } catch (e) {
+    res.status(500).json({ erro: e.message });
+  }
+}
