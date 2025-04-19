d32f25d6d4eea33546c18555609dd81063550849
ConectaServe <conectaserve@gmail.com>
2025-04-18 23:44:48 -0300

    Update editarUsuario.js

--- api/editarUsuario.js
+++ api/editarUsuario.js
@@ -11,21 +11,17 @@ if (!getApps().length) {
 const db = getFirestore();
 
 export default async function handler(req, res) {
-  const { id, nome, cpf, email, tipo } = req.query;
-
-  if (req.method === "PATCH") {
-    try {
-      await db.collection("usuarios").doc(id).update({
-        nome,
-        cpf,
-        email,
-        tipo
-      });
-      return res.status(200).json({ sucesso: true });
-    } catch (error) {
-      return res.status(500).json({ erro: error.message });
-    }
-  } else {
+  if (req.method !== "PATCH") {
     return res.status(405).json({ erro: "Método não permitido" });
+  }
+
+  const { id } = req.query;
+  const { nome, email, cpf, tipo } = req.body;
+
+  try {
+    await db.collection("usuarios").doc(id).update({ nome, email, cpf, tipo });
+    return res.status(200).json({ sucesso: true });
+  } catch (error) {
+    return res.status(500).json({ erro: error.message });
   }
 }
